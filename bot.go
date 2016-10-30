cf75bef6343a887a53ed08e22750185f46470363
Alex Nichol <unixpickle@gmail.com>
2016-10-29 20:30:58 -0400

    created training program for bot

--- bot.go
+++ bot.go
@@ -1,7 +1,11 @@
 package chatbot
 
 import (
+	"fmt"
+	"io/ioutil"
+
 	"github.com/unixpickle/num-analysis/linalg"
+	"github.com/unixpickle/serializer"
 	"github.com/unixpickle/weakai/neuralnet"
 	"github.com/unixpickle/weakai/rnn"
 )
@@ -42,6 +46,31 @@ func NewBot() *Bot {
 	}
 	fullNet = append(fullNet, outBlock)
 	return &Bot{Block: fullNet}
+}
+
+// LoadBot reads a Bot from a file.
+func LoadBot(path string) (*Bot, error) {
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	decoded, err := serializer.DeserializeWithType(contents)
+	if err != nil {
+		return nil, err
+	}
+	if block, ok := decoded.(rnn.Block); ok {
+		return &Bot{Block: block}, nil
+	}
+	return nil, fmt.Errorf("type is not an rnn.Block: %T", decoded)
+}
+
+// Save saves the Bot to a file.
+func (b *Bot) Save(path string) error {
+	encoded, err := serializer.SerializeWithType(b.Block.(serializer.Serializer))
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(path, encoded, 0755)
 }
 
 func oneHotVector(i int) linalg.Vector {
---
+++ train/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/unixpickle/chatbot"
+	"github.com/unixpickle/sgd"
+	"github.com/unixpickle/weakai/neuralnet"
+	"github.com/unixpickle/weakai/rnn"
+	"github.com/unixpickle/weakai/rnn/seqtoseq"
+)
+
+const (
+	MaxBufferChars = 400
+	StepSize       = 0.001
+	BatchSize      = 16
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, "Usage: train <samples> <output>")
+		os.Exit(1)
+	}
+
+	samples, err := chatbot.NewSampleSet(os.Args[1], MaxBufferChars)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to load samples:", err)
+		os.Exit(1)
+	} else if samples.Len() == 0 {
+		fmt.Fprintln(os.Stderr, "No samples loaded.")
+		os.Exit(1)
+	}
+
+	bot, err := chatbot.LoadBot(os.Args[2])
+	if os.IsNotExist(err) {
+		log.Println("Creating bot...")
+		bot = chatbot.NewBot()
+	} else if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to load bot:", err)
+		os.Exit(1)
+	}
+
+	log.Println("Partitioning", samples.Len(), "samples...")
+	training, validation := sgd.HashSplit(samples, 0.9)
+
+	log.Println("Training...")
+
+	costFunc := neuralnet.DotCost{}
+	gradienter := &sgd.Adam{
+		Gradienter: &seqtoseq.Gradienter{
+			SeqFunc:  &rnn.BlockSeqFunc{B: bot.Block},
+			Learner:  bot.Block.(sgd.Learner),
+			CostFunc: costFunc,
+		},
+	}
+
+	var iteration int
+	var lastBatch sgd.SampleSet
+	sgd.SGDMini(gradienter, training, StepSize, BatchSize, func(s sgd.SampleSet) bool {
+		var lastCost float64
+		if lastBatch != nil {
+			lastCost = seqtoseq.TotalCostBlock(bot.Block, BatchSize, lastBatch, costFunc)
+		}
+		lastBatch = s.Copy()
+		newCost := seqtoseq.TotalCostBlock(bot.Block, BatchSize, s, costFunc)
+
+		sgd.ShuffleSampleSet(validation)
+		validationCost := seqtoseq.TotalCostBlock(bot.Block, BatchSize,
+			validation.Subset(0, BatchSize), costFunc)
+
+		log.Printf("iter %d: validation=%f cost=%f last=%f", iteration, validationCost,
+			newCost, lastCost)
+
+		iteration++
+		return true
+	})
+
+	if err := bot.Save(os.Args[2]); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to save output:", err)
+		os.Exit(1)
+	}
+}
